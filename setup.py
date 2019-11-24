1b2049079858853d6f298a5edda968ac4b0ca8f7
Michael Marchetti <mike@rstudio.com>
2019-11-23 20:30:47 -0500

    add console script entrypoint

--- setup.py
+++ setup.py
@@ -31,5 +31,9 @@ setup(name='rsconnect_python',
         'six',
         'click',
     ],
-    python_requires = '>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*'
+    python_requires = '>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+    entry_points='''
+        [console_scripts]
+        rsconnect=rsconnect.main:cli
+    '''
 )
