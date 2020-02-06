72643bb2d06bce8d468a34d546dddd5c87e9a598
Scott Jackson <scottmmjackson@gmail.com>
2020-02-06 13:50:29 -0500

    add deploy test with optional cacert argument

---
+++ rsconnect/tests/test_data_util.py
@@ -0,0 +1,11 @@
+import sys
+from os.path import join, dirname, exists
+
+
+def get_dir(name):
+    py_version = 'py%d' % sys.version_info[0]
+    # noinspection SpellCheckingInspection
+    path = join(dirname(__file__), 'testdata', py_version, name)
+    if not exists(path):
+        raise AssertionError('%s does not exist' % path)
+    return path
