3fd90067819ecd569657f8dff2c46ea40e3a15d1
Bincheng Wu <bincheng.wu@rstudio.com>
2022-04-05 13:04:08 -0400

    fix deploying with --new

--- rsconnect/actions.py
+++ rsconnect/actions.py
@@ -1066,23 +1066,24 @@ def gather_basic_deployment_info_for_html(connect_server, app_store, path, new, 
 
     app_mode = AppModes.STATIC
     existing_app_mode = None
-    if app_id is None:
-        # Possible redeployment - check for saved metadata.
-        # Use the saved app information unless overridden by the user.
-        app_id, existing_app_mode = app_store.resolve(connect_server.url, app_id, app_mode)
-        logger.debug("Using app mode from app %s: %s" % (app_id, app_mode))
-    else:
-        # Don't read app metadata if app-id is specified. Instead, we need
-        # to get this from Connect.
-        app = api.get_app_info(connect_server, app_id)
-        existing_app_mode = AppModes.get_by_ordinal(app.get("app_mode", 0), True)
-    if not new and existing_app_mode and app_mode != existing_app_mode:
-        msg = (
-            "Deploying with mode '%s',\n"
-            + "but the existing deployment has mode '%s'.\n"
-            + "Use the --new option to create a new deployment of the desired type."
-        ) % (app_mode.desc(), existing_app_mode.desc())
-        raise api.RSConnectException(msg)
+    if not new:
+        if app_id is None:
+            # Possible redeployment - check for saved metadata.
+            # Use the saved app information unless overridden by the user.
+            app_id, existing_app_mode = app_store.resolve(connect_server.url, app_id, app_mode)
+            logger.debug("Using app mode from app %s: %s" % (app_id, app_mode))
+        elif app_id:
+            # Don't read app metadata if app-id is specified. Instead, we need
+            # to get this from Connect.
+            app = api.get_app_info(connect_server, app_id)
+            existing_app_mode = AppModes.get_by_ordinal(app.get("app_mode", 0), True)
+        if existing_app_mode and app_mode != existing_app_mode:
+            msg = (
+                "Deploying with mode '%s',\n"
+                + "but the existing deployment has mode '%s'.\n"
+                + "Use the --new option to create a new deployment of the desired type."
+            ) % (app_mode.desc(), existing_app_mode.desc())
+            raise api.RSConnectException(msg)
 
     default_title = not bool(title)
     title = title or _default_title(path)
