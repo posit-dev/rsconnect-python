e47789ec350d369bc53fbbea0429e00a584eccce
isabelizimm <isabel.zimmerman@rstudio.com>
2022-10-13 15:06:10 -0400

    adding initial files to set up testing

--- Makefile
+++ Makefile
@@ -171,3 +171,20 @@ promote-docs-in-s3:
 		--cache-control max-age=300 \
 		docs/site/ \
 		s3://docs.rstudio.com/rsconnect-python/
+
+
+dev: vetiver/tests/rsconnect_api_keys.json
+
+dev-start:
+	docker-compose up -d
+	docker-compose exec -T rsconnect bash < script/setup-rsconnect/add-users.sh
+	# curl fails with error 52 without a short sleep....
+	sleep 5
+	curl -s --retry 10 --retry-connrefused http://localhost:3939
+
+dev-stop:
+	docker-compose down
+	rm -f $(RSC_API_KEYS)
+
+$(RSC_API_KEYS): dev-start
+	python script/setup-rsconnect/dump_api_keys.py $@
---
+++ tests/test_vetiver_pins.py
@@ -0,0 +1,94 @@
+import pytest
+import json
+import sklearn
+import pins
+from pins.boards import BoardRsConnect
+from pins.rsconnect.api import RsConnectApi
+from pins.rsconnect.fs import RsConnectFs
+from rsconnect.api import RSConnectServer
+
+import vetiver
+
+# Load data, model
+X_df, y = vetiver.mock.get_mock_data()
+model = vetiver.mock.get_mock_model().fit(X_df, y)
+
+RSC_SERVER_URL = "http://localhost:3939"
+RSC_KEYS_FNAME = "vetiver/tests/rsconnect_api_keys.json"
+
+pytestmark = pytest.mark.rsc_test  # noqa
+
+
+def server_from_key(name):
+    with open(RSC_KEYS_FNAME) as f:
+        api_key = json.load(f)[name]
+        return RSConnectServer(RSC_SERVER_URL, api_key)
+
+
+def get_key(name):
+    with open(RSC_KEYS_FNAME) as f:
+        api_key = json.load(f)[name]
+        return api_key
+
+
+def rsc_from_key(name):
+    with open(RSC_KEYS_FNAME) as f:
+        api_key = json.load(f)[name]
+        return RsConnectApi(RSC_SERVER_URL, api_key)
+
+
+def rsc_fs_from_key(name):
+
+    rsc = rsc_from_key(name)
+
+    return RsConnectFs(rsc)
+
+
+def rsc_delete_user_content(rsc):
+    guid = rsc.get_user()["guid"]
+    content = rsc.get_content(owner_guid=guid)
+    for entry in content:
+        rsc.delete_content_item(entry["guid"])
+
+
+@pytest.fixture(scope="function")
+def rsc_short():
+    # tears down content after each test
+    fs_susan = rsc_fs_from_key("susan")
+
+    # delete any content that might already exist
+    rsc_delete_user_content(fs_susan.api)
+
+    yield BoardRsConnect(
+        "", fs_susan, allow_pickle_read=True
+    )  # fs_susan.ls to list content
+
+    rsc_delete_user_content(fs_susan.api)
+
+
+def test_board_pin_write(rsc_short):
+    v = vetiver.VetiverModel(
+        model=model, ptype_data=X_df, model_name="susan/model", versioned=None
+    )
+    vetiver.vetiver_pin_write(board=rsc_short, model=v)
+    assert isinstance(rsc_short.pin_read("susan/model"), sklearn.dummy.DummyRegressor)
+
+
+def test_deploy(rsc_short):
+    v = vetiver.VetiverModel(
+        model=model, ptype_data=X_df, model_name="susan/model", versioned=None
+    )
+    board = pins.board_rsconnect(
+        server_url=RSC_SERVER_URL, api_key=get_key("susan"), allow_pickle_read=True
+    )
+
+    vetiver.vetiver_pin_write(board=board, model=v)
+    connect_server = RSConnectServer(url=RSC_SERVER_URL, api_key=get_key("susan"))
+    assert isinstance(board.pin_read("susan/model"), sklearn.dummy.DummyRegressor)
+
+    vetiver.deploy_rsconnect(
+        connect_server=connect_server, board=board, pin_name="susan/model"
+    )
+    response = vetiver.predict(RSC_SERVER_URL + "/predict", json=X_df)
+    assert response.status_code == 200, response.text
+    assert response.json() == {"prediction": [44.47, 44.47]}, response.json()
