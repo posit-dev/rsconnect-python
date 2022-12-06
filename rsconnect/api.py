4868b9f35630147111141d725f2a28c93a339f59
Bincheng Wu <bincheng.wu@rstudio.com>
2022-12-06 12:07:43 -0500

    allow redeployment to unknown app mode

--- rsconnect/api.py
+++ rsconnect/api.py
@@ -852,7 +852,7 @@ class RSConnectExecutor:
                     existing_app_mode = AppModes.get_by_cloud_name(app.json_data["mode"])
                 else:
                     raise RSConnectException("Unable to infer Connect client.")
-            if existing_app_mode and app_mode != existing_app_mode:
+            if existing_app_mode and app_mode != existing_app_mode and existing_app_mode != "unknown":
                 msg = (
                     "Deploying with mode '%s',\n"
                     + "but the existing deployment has mode '%s'.\n"
