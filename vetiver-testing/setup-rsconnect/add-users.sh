fa0884263dab6c124024ca8cf4e59157e3cadde2
Toph Allen <toph.allen@gmail.com>
2023-07-07 17:00:00 -0400

    Add Taylor’s fix

--- vetiver-testing/setup-rsconnect/add-users.sh
+++ vetiver-testing/setup-rsconnect/add-users.sh
@@ -1,1 +1,4 @@
+sudo apt-get -y update
+sudo apt-get -y --reinstall install cracklib-runtime
+
 awk ' { system("useradd -m -s /bin/bash "$1); system("echo \""$1":"$2"\" | chpasswd"); system("id "$1) } ' /etc/users.txt
