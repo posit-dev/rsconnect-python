8d2ee1e797973ca4c85c886ab925fd48e0614bda
tdstein <taylor.steinberg@posit.co>
2023-08-01 12:07:00 -0400

    Alters passwords to resolve BAD PASSWORD errors.

--- vetiver-testing/setup-rsconnect/dump_api_keys.py
+++ vetiver-testing/setup-rsconnect/dump_api_keys.py
@@ -13,9 +13,9 @@ def get_api_key(user, password, email):
 
 
 api_keys = {
-    "admin": get_api_key("admin", "admin0", "admin@example.com"),
-    "susan": get_api_key("susan", "susan", "susan@example.com"),
-    "derek": get_api_key("derek", "derek", "derek@example.com"),
+    "admin": get_api_key("admin", "MjZIfyBtzAFzbfVv", "admin@example.com"),
+    "susan": get_api_key("susan", "izADvXATrZLyoXar", "susan@example.com"),
+    "derek": get_api_key("derek", "DUQtloSqrSFHKflA", "derek@example.com"),
 }
 
 json.dump(api_keys, open(OUT_FILE, "w"))
