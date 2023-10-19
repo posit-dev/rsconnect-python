691d62bef02f5f9a6181762697db8e2a7f20add3
Bill Sager <whs@sager.org>
2023-10-19 10:49:43 -0700

    compatibility with previous saved environments

--- rsconnect/environment.py
+++ rsconnect/environment.py
@@ -49,6 +49,7 @@ def MakeEnvironment(
     pip: Optional[str] = None,
     python: Optional[str] = None,
     source: Optional[str] = None,
+    conda: Optional[str] = None,  # Deprecated, but included for compatibility with past environments
 ):
     return Environment(contents, error, filename, locale, package_manager, pip, python, source)
 
