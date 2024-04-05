d52f4e876e6ec0c7f41c1345359bd1cb84501898
Kevin Gartland <kevin.gartland@posit.co>
2024-04-05 12:41:39 -0400

    increase wait

--- vetiver-testing/wait_for_connect.sh
+++ vetiver-testing/wait_for_connect.sh
@@ -1,4 +1,4 @@
-timeout 5 bash -c \
+timeout 30 bash -c \
     'status=$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost:3939/__ping__); \
     while [[ "$status" != "200" ]]; \
         do sleep 1; \
