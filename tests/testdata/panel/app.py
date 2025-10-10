478f10f0adbec4f07933feabd6a794c3e4a7ae0f
Matt Conflitti <matt.conflitti@posit.co>
2025-10-10 16:58:16 -0400

    formatting

--- tests/testdata/panel/app.py
+++ tests/testdata/panel/app.py
@@ -2,24 +2,23 @@ import panel as pn
 
 pn.extension()
 
+
 def greet(name):
     return f"Hello, {name}!"
+
 
 text_input = pn.widgets.TextInput(name="Enter your name", placeholder="Type here...")
 button = pn.widgets.Button(name="Greet", button_type="primary")
 
 output = pn.pane.Markdown("Click the button to see a greeting!")
 
+
 def update_output(event):
     output.object = greet(text_input.value)
 
+
 button.on_click(update_output)
 
-app = pn.Column(
-    "# Panel Greeting App",
-    text_input,
-    button,
-    output
-)
+app = pn.Column("# Panel Greeting App", text_input, button, output)
 
 app.servable()
