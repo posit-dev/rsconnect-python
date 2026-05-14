3205a39e1a10148e66707eea345c1fff587f2ed5
Alessandro Molina <alessandro.molina@posit.co>
2026-05-14 16:32:16 +0200

    encapsulate templates

---
+++ rsconnect/quickstart/__init__.py
@@ -0,0 +1,5 @@
+"""``rsconnect quickstart`` package.
+
+The scaffolding implementation lives in :mod:`rsconnect.quickstart.quickstart`.
+Templates ship as package data under :mod:`rsconnect.quickstart.templates`.
+"""
